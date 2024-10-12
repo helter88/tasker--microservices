22cd6cfe8a5c19989378030b50ddb0193b8f4b00
Artur <artur.helta@onet.pl>
2024-10-12 09:48:06 +0200

    remove CommandRunnerConfig and MySqlConfig in queue-manager

--- queue-manager/src/main/java/com/example/queue_manager/config/MySqlConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.queue_manager.config;
-
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EntityScan("com.example.queue_manager.model")
-public class MySqlConfig {
-}
